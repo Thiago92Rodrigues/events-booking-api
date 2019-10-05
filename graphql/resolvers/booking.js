02b9130c71ef9f14b817719f51c50ff5e8694bd6
Thiago Rodrigues <45070690+Thiago92Rodrigues@users.noreply.github.com>
2019-10-05 10:57:36 -0300

    add booking resolver

---
+++ graphql/resolvers/booking.js
@@ -0,0 +1,67 @@
+const Booking = require('../../models/booking');
+const Event = require('../../models/event');
+const { transformBooking, transformEvent } = require('./util');
+
+module.exports = {
+
+    /**
+     * This function returns all bookings registered in the database
+     */
+    bookings: async (args, req) => {
+        // verify if the user is authenticated
+        if (!req.isAuthenticated) throw new Error('User is unauthenticated.');
+
+        try {
+            const bookings = await Booking.find();
+            return bookings.map(booking => {
+                return transformBooking(booking);
+            });
+
+        } catch (error) {
+            throw error;
+        }
+    },
+
+    /**
+     * This function creates a new booking from an existing event
+     */
+    bookEvent: async (args, req) => {
+        // verify if the user is authenticated
+        if (!req.isAuthenticated) throw new Error('User is unauthenticated.');
+
+        try {
+            const fetchedEvent = await Event.findOne({ _id: args.eventId });
+            const booking = new Booking({
+                user: req.userId,
+                event: fetchedEvent,
+            });
+
+            const result = await booking.save();
+            return transformBooking(result);
+
+        } catch (error) {
+            throw error;
+        }
+    },
+
+    /**
+     * This function deletes an existing booking
+     */
+    cancelBooking: async (args, req) => {
+        // verify if the user is authenticated
+        if (!req.isAuthenticated) throw new Error('User is unauthenticated.');
+
+        try {
+            const booking = await Booking.findById(args.bookingId).populate('event');
+            const event = transformEvent(booking.event);
+
+            await Booking.deleteOne({ _id: args.bookingId });
+
+            return event;
+
+        } catch (error) {
+            throw error;
+        }
+    },
+
+};
