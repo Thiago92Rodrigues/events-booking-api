1a35eeb8276b1bd4083f28b34bee584352f7da67
Thiago Rodrigues <45070690+Thiago92Rodrigues@users.noreply.github.com>
2019-10-24 15:34:54 -0300

    add logger middleware

---
+++ middleware/logger.js
@@ -0,0 +1,23 @@
+const morgan = require('morgan');
+
+const logger = morgan(function(tokens, req, res) {
+  return [
+    '[',
+    tokens.method(req, res),
+    ']',
+    '"',
+    tokens.url(req, res),
+    '"',
+    tokens.date(req, res, 'web'),
+
+    'STATUS:',
+    tokens.status(req, res),
+    tokens.res(req, res, 'content-length'),
+    '-',
+    'response time:',
+    tokens['response-time'](req, res),
+    'ms'
+  ].join(' ');
+});
+
+module.exports.logger = logger;
