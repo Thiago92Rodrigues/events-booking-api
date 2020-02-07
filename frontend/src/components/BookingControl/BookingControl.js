7c71958119bc3f842c8d06e8549b658637b4a258
Thiago92Rodrigues <thiago.clrodrigues@gmail.com>
2020-02-06 23:59:42 -0300

    add booking list, booking control and booking chart components

---
+++ frontend/src/components/BookingControl/BookingControl.js
@@ -0,0 +1,26 @@
+import React from 'react';
+
+// Style
+import './BookingControl.css';
+
+const bookingControl = (props) => {
+  return (
+    <div className="booking__control">
+      <button
+        className={props.activeOutputType === 'list' ? 'active' : ''}
+        onClick={props.onChange.bind(this, 'list')}
+      >
+        List
+      </button>
+
+      <button
+        className={props.activeOutputType === 'chart' ? 'active' : ''}
+        onClick={props.onChange.bind(this, 'chart')}
+      >
+        Chart
+      </button>
+    </div>
+  );
+};
+
+export default bookingControl;
---
+++ frontend/src/components/BookingList/BookingList.js
@@ -0,0 +1,29 @@
+import React from 'react';
+
+// Style
+import './BookingList.css';
+
+const bookingList = (props) => (
+  <ul className="booking__list">
+    {props.bookings.map((booking) => {
+      return (
+        <li key={booking._id} className="booking__list__item">
+          <div className="booking__item__data">
+            {booking.event.title} -{' '}
+            {new Date(booking.createdAt).toLocaleDateString()}
+          </div>
+          <div className="booking__item__actions">
+            <button
+              className="btn"
+              onClick={props.onDelete.bind(this, booking._id)}
+            >
+              Cancel
+            </button>
+          </div>
+        </li>
+      );
+    })}
+  </ul>
+);
+
+export default bookingList;
