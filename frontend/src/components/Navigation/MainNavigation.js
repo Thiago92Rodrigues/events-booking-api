f52cee49846b89d4654fc621878713222570892e
Thiago92Rodrigues <thiago.clrodrigues@gmail.com>
2020-02-06 23:58:47 -0300

    add main navigation and modal components

---
+++ frontend/src/components/Navigation/MainNavigation.js
@@ -0,0 +1,52 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+
+// Contexts
+import AuthContext from '../../context/auth-context';
+
+// Style
+import './MainNavigation.css';
+
+const mainNavigation = (props) => (
+  <AuthContext.Consumer>
+    {(context) => {
+      return (
+        <header className="main__navbar">
+          <div className="main__navbar__logo">
+            <h1>The NavBar</h1>
+          </div>
+
+          <nav className="main__navbar__items">
+            <ul>
+              {/* authentication tab only appears if there is no token */}
+              {!context.token && (
+                <li>
+                  <NavLink to="/auth">Authentication</NavLink>
+                </li>
+              )}
+
+              <li>
+                <NavLink to="/events">Events</NavLink>
+              </li>
+
+              {/* bookings tab only appears if there is a token */}
+              {context.token && (
+                <li>
+                  <NavLink to="/bookings">Bookings</NavLink>
+                </li>
+              )}
+
+              {context.token && (
+                <li>
+                  <button onClick={context.logout}>Logout</button>
+                </li>
+              )}
+            </ul>
+          </nav>
+        </header>
+      );
+    }}
+  </AuthContext.Consumer>
+);
+
+export default mainNavigation;
