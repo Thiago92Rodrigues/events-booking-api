f7a420f875db6ba71fdd87785fb5a89af77ef100
Thiago92Rodrigues <thiago.clrodrigues@gmail.com>
2020-03-11 23:40:27 -0300

    place state inside constructor and rename vars in Auth Page

--- frontend/src/pages/Auth.js
+++ frontend/src/pages/Auth.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Component } from 'react';
 
-// Contexts
+// Context
 import AuthContext from '../context/authContext';
 
 // Style
@@ -15,45 +15,44 @@ import {
 import { sendRequest } from '../utils/requestsAPI';
 
 class AuthPage extends Component {
-  state = {
-    // in which mode the user is: login mode or signup mode
-    is_login: true
-  };
-
   static contextType = AuthContext;
 
   constructor(props) {
     super(props);
-    this.email_element = React.createRef();
-    this.password_element = React.createRef();
+    this.state = {
+      isLoginMode: true // in which mode the user is: login mode or signup mode
+    };
+
+    this.emailElement = React.createRef();
+    this.passwordElement = React.createRef();
   }
 
   switchModeHandler = () => {
     this.setState((prevState) => {
-      return { is_login: !prevState.is_login };
+      return { isLoginMode: !prevState.isLoginMode };
     });
   };
 
   submitHandler = (event) => {
     event.preventDefault();
 
-    const email = this.email_element.current.value;
-    const password = this.password_element.current.value;
+    const email = this.emailElement.current.value;
+    const password = this.passwordElement.current.value;
 
     // check if the inputs are valid
     if (email.trim().length === 0 || password.trim().length === 0) return;
 
-    let request_body;
+    let requestBody;
 
-    if (this.state.is_login) {
+    if (this.state.isLoginMode) {
       console.log('LOGIN');
-      request_body = buildLoginRequest(email, password);
+      requestBody = buildLoginRequest(email, password);
     } else {
       console.log('CREATE USER');
-      request_body = buildCreateUserRequest(email, password);
+      requestBody = buildCreateUserRequest(email, password);
     }
 
-    sendRequest(request_body)
+    sendRequest(requestBody)
       .then((response) => {
         if (response.status !== 200 && response.status !== 201)
           throw new Error('Failed');
@@ -61,6 +60,7 @@ class AuthPage extends Component {
       })
       .then((response) => {
         console.log('response ', response);
+
         if (response.data.login) {
           this.context.login(
             response.data.login.token,
@@ -78,18 +78,18 @@ class AuthPage extends Component {
     return (
       <React.Fragment>
         <span className="auth__header">
-          {this.state.is_login ? 'Login' : 'Sign Up'}
+          {this.state.isLoginMode ? 'Login' : 'Sign Up'}
         </span>
 
         <form className="auth__form" onSubmit={this.submitHandler}>
           <div className="form-control">
             <label htmlFor="email">Email</label>
-            <input type="email" id="email" ref={this.email_element} />
+            <input type="email" id="email" ref={this.emailElement} />
           </div>
 
           <div className="form-control">
             <label htmlFor="password">Password</label>
-            <input type="password" id="password" ref={this.password_element} />
+            <input type="password" id="password" ref={this.passwordElement} />
           </div>
 
           <div className="form-actions">
@@ -97,7 +97,7 @@ class AuthPage extends Component {
 
             {/* Switch page's mode button */}
             <button type="button" onClick={this.switchModeHandler}>
-              Switch to {this.state.is_login ? 'Sign Up' : 'Login'}
+              Switch to {this.state.isLoginMode ? 'Sign Up' : 'Login'}
             </button>
           </div>
         </form>
